083cbe308a909e543e5d0616ccfa9404c7e277ea
Ndiforamang Fusi <fusisoft@gmail.com>
2025-11-05 15:30:05 -0600

    updated jenkins file

--- prod/backend.tf
+++ prod/backend.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = ">=0.12.0"
   backend "s3" {
     key            = "prod/terraform.state"
-    bucket         = "class39-terraform-backend-bucket"
-    region         = "us-east-2"
-    dynamodb_table = "terraform-state-locking"
+    bucket         = "dominion-terraform-backend"
+    region         = "us-west-2"
+    dynamodb_table = "terraform-tooling-state-locking"
   }
 }
